b43c480598b1633a660fe25413b07525447c3a2a
Ashaluwala Kazeem <ashaluwalakazeem@gmail.com>
2024-08-05 19:09:04 +0100

    project set up

---
+++ app/src/main/java/com/milsat/capstone/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.milsat.capstone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.milsat.capstone.ui.theme.CapstoneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CapstoneTheme {
+
+            }
+        }
+    }
+}
